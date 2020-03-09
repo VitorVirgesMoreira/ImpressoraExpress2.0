1f9c9aa18f214bddb328739b23f05657151a9e5c
Carlos Oliveira <Caik_oliveirapires@hotmail.com>
2020-03-09 16:59:38 -0300

    Peita pa ve

---
+++ BLL/Validator/EnderecoValidator.cs
@@ -0,0 +1,24 @@
+﻿using DTO.ComplexTypes;
+using Microsoft.Build.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Validator
+{
+    public class EnderecoValidator
+    {
+        public List<Error> Validate(Endereco endereco)
+        {
+            List<Error> errors = new List<Error>();
+
+            if (string.IsNullOrWhiteSpace(endereco.Bairro))
+            {
+                errors.Add(new Error() { FieldName = "Bairro", Message = "Bairro deve ser informado." });
+            }
+            else if (string,IsNullOrWhiteSpace(endereco.Rua)
+            //FAZER DE TODOS OS CAMPOS
+            return errors;
+        }
+    }
+}
---
+++ Common/NecoException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    [Serializable]
+    public class NecoException : Exception
+    {
+        //Aqui deveria haver um Task também??
+        //Provavelmente iremos ter que fazer a Exception.
+        public List<Error> Errors { get; private set; }
+
+    public NecoException(List<Error> errors)
+    {
+        this.Errors = errors;
+    }
+
+    //Daqui pra baixo é apenas código que o proprio VS gera 
+    //pra gente poder utilizar esta exceção 
+    public NecoException(string message) : base(message) { }
+    public NecoException(string message, Exception inner) : base(message, inner) { }
+    protected NecoException(
+      System.Runtime.Serialization.SerializationInfo info,
+      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
+    
+}
