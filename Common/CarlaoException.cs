f00eb9bc93fa1ef16aae720edccfb18e5d240380
vitor virges <vitor.virges@gmail.com>
2020-03-09 17:09:00 -0300

    aa

--- Common/CarlaoException.cs
+++ Common/CarlaoException.cs
@@ -5,22 +5,22 @@ using System.Text;
 namespace Common
 {
     [Serializable]
-    public class NecoException : Exception
+    public class CarlaoException : Exception
     {
         //Aqui deveria haver um Task também??
         //Provavelmente iremos ter que fazer a Exception.
         public List<Error> Errors { get; private set; }
 
-    public NecoException(List<Error> errors)
+    public CarlaoException(List<Error> errors)
     {
         this.Errors = errors;
     }
 
     //Daqui pra baixo é apenas código que o proprio VS gera 
     //pra gente poder utilizar esta exceção 
-    public NecoException(string message) : base(message) { }
-    public NecoException(string message, Exception inner) : base(message, inner) { }
-    protected NecoException(
+    public CarlaoException(string message) : base(message) { }
+    public CarlaoException(string message, Exception inner) : base(message, inner) { }
+    protected CarlaoException(
       System.Runtime.Serialization.SerializationInfo info,
       System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 }
